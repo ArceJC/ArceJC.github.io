4c3bac9948a0135f44782bc6e6575c509ac99c7c
ArceJC <144378088+ArceJC@users.noreply.github.com>
2023-09-07 10:54:29 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript
+document.addEventListener('DOMContentLoaded', function () {
+    var miBoton = document.getElementById('miBoton');
+
+    miBoton.addEventListener('click', function () {
+        alert('¡Has hecho clic en el botón!');
+    });
+});
