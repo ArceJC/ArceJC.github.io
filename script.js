9d64465156a96d2945d7feb0e0824a4503cdfcf6
ArceJC <144378088+ArceJC@users.noreply.github.com>
2023-09-13 19:03:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// JavaScript
-document.addEventListener('DOMContentLoaded', function () {
-    var miBoton = document.getElementById('miBoton');
+// Validación del formulario
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('mensaje').value;
 
-    miBoton.addEventListener('click', function () {
-        alert('¡Has hecho clic en el botón!');
-    });
+    if (nombre.trim() === '' || email.trim() === '' || mensaje.trim() === '') {
+        alert('Por favor, complete todos los campos.');
+    } else {
+        alert('Formulario enviado con éxito.');
+        form.reset();
+    }
 });
